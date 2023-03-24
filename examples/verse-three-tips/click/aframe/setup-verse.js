aa0df1f7dbbb2687fba64b106dd0174119e3ac25
hosoi-appland <hosoi+verse@appland.co.jp>
2023-03-24 13:20:22 +0900

    How To Detect Click

---
+++ examples/verse-three-tips/click/aframe/setup-verse.js
@@ -0,0 +1,120 @@
+(() => {
+  const { VerseThree, THREE } = window;
+  const VERSE_WASM_URL =
+    "https://static.verseengine.cloud/examples/verse-three/dist/verse_core_bg.wasm";
+  const ENTRANCE_SERVER_URL = "https://entrance.verseengine.cloud";
+  const ANIMATION_MAP = {
+    idle: "/examples/asset/animation/idle.fbx",
+    walk: "/examples/asset/animation/walk.fbx",
+  };
+  const range = (n) => [...Array(n).keys()];
+  const PRESET_AVATARS = [
+    ...range(3).map((i) => `f${i}`),
+    ...range(3).map((i) => `m${i}`),
+  ].map((n) => ({
+    thumbnailURL: `/examples/asset/avatar/${n}.png`,
+    avatarURL: `/examples/asset/avatar/${n}.vrm`,
+  }));
+  const DEFAULT_AVATAR_URL =
+    PRESET_AVATARS[Math.floor(Math.random() * PRESET_AVATARS.length)].avatarURL;
+  const ICE_SERVERS = [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun1.l.google.com:19302",
+    },
+  ];
+
+  async function waitForAframeLoad(scene) {
+    if (scene.hasLoaded) {
+      return;
+    }
+    return new Promise((resolve) => scene.addEventListener("loaded", resolve));
+  }
+
+  main();
+
+  function main() {
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", _main);
+    } else {
+      _main();
+    }
+  }
+
+  async function _main() {
+    const scene = document.querySelector("a-scene");
+    await waitForAframeLoad(scene);
+
+    let collisionObjects = [];
+    let teleportTargetObjects = [];
+    let interactableObjects = [];
+    let collisionBoxes = [];
+
+    const updateCollisionBoxes = () => {
+      collisionBoxes.length = 0;
+      [...collisionObjects, ...teleportTargetObjects].map((el) => {
+        el.traverse((c) => {
+          if (!c.isMesh) {
+            return;
+          }
+          collisionBoxes.push(new THREE.Box3().setFromObject(c));
+        });
+      });
+    };
+    const queryObjects = (q) =>
+      [...scene.querySelectorAll(q)]
+        .map((v) => v.object3D)
+        .filter((v) => v.visible);
+    const updateCollisionObjects = () => {
+      collisionObjects = queryObjects(".collider,.wall");
+      teleportTargetObjects = queryObjects(".ground,.environmentGround");
+      interactableObjects = queryObjects(".clickable");
+      updateCollisionBoxes();
+    };
+    scene.object3D.updateMatrixWorld();
+    updateCollisionObjects();
+
+    const adapter = new VerseThree.AFrameEnvAdapter(
+      scene,
+      document.getElementById("headOffset").object3D,
+      document.getElementById("player").object3D,
+      () => collisionBoxes,
+      () => collisionObjects,
+      () => teleportTargetObjects,
+      {
+        getInteractableObjects: () => interactableObjects,
+        onSelectDown: (o, _point) => {
+          if (!o.el || !o.el.classList.contains("clickable")) {
+            return;
+          }
+          o.el.emit("click", {
+            intersectedEl: o.el,
+          });
+        },
+        isLowSpecMode: true,
+      }
+    );
+    const mayBeLowSpecDevice = VerseThree.isLowSpecDevice();
+    const res = await VerseThree.start(
+      adapter,
+      scene.object3D,
+      VERSE_WASM_URL,
+      ENTRANCE_SERVER_URL,
+      DEFAULT_AVATAR_URL,
+      ANIMATION_MAP,
+      ICE_SERVERS,
+      {
+        maxNumberOfPeople: mayBeLowSpecDevice ? 8 : 16,
+        maxNumberOfParallelFileTransfers: mayBeLowSpecDevice ? 1 : 4,
+        presetAvatars: PRESET_AVATARS,
+      }
+    );
+
+    // @see index.html/extend-animation-loop
+    window.__extendAnimationLoop = (_time, timeDelta) => {
+      res.tick(timeDelta / 1000);
+    };
+  }
+})();
---
+++ examples/verse-three-tips/click/three/index.js
@@ -0,0 +1,150 @@
+import * as THREE from "three";
+import { Sky } from "three/examples/jsm/objects/Sky.js";
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
+import { setupVerse } from "./setup-verse";
+
+function setupScene(ticks) {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  if ("xr" in navigator) {
+    navigator.xr.isSessionSupported("immersive-vr").then(function (supported) {
+      if (supported) {
+        renderer.xr.enabled = true;
+
+        document.addEventListener("keydown", function (e) {
+          if (e.key === "Escape") {
+            if (renderer.xr.isPresenting) {
+              renderer.xr.getSession()?.end();
+            }
+          }
+        });
+        const vrButton = VRButton.createButton(renderer);
+        document.body.appendChild(vrButton);
+      }
+    });
+  } else {
+    if (window.isSecureContext === false) {
+      console.warn("webxr needs https");
+    } else {
+      console.warn("webxr not available");
+    }
+  }
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+  );
+  camera.position.set(0.0, 1.6, 0);
+  scene.add(camera);
+
+  scene.add(new THREE.AmbientLight(0xffffff, 0.4));
+  const light = new THREE.DirectionalLight(0xffffff, 0.6);
+  light.position.set(0, 10, -10).normalize();
+  scene.add(light);
+
+  {
+    const sky = new Sky();
+    sky.scale.setScalar(450000);
+    scene.add(sky);
+
+    const sun = new THREE.Vector3();
+    sun.setFromSphericalCoords(
+      1,
+      THREE.MathUtils.degToRad(60),
+      THREE.MathUtils.degToRad(180)
+    );
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+  }
+  let ground;
+  {
+    ground = new THREE.Mesh(
+      new THREE.PlaneGeometry(50, 50, 1, 1),
+      new THREE.MeshLambertMaterial({
+        color: 0x5e5e5e,
+      })
+    );
+    ground.rotation.x = Math.PI / -2;
+    scene.add(ground);
+  }
+  scene.add(new THREE.GridHelper(50, 50));
+
+  ticks ||= [];
+  const clock = new THREE.Clock();
+  const animate = () => {
+    const dt = clock.getDelta();
+    ticks.forEach((f) => f(dt));
+    renderer.render(scene, camera);
+  };
+  renderer.setAnimationLoop(animate);
+
+  return { scene, renderer, camera, ground };
+}
+
+function main() {
+  const ticks = [];
+  const { scene, renderer, camera, ground } = setupScene(ticks);
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshStandardMaterial({
+      side: THREE.DoubleSide,
+      color: 0xff6347,
+    })
+  );
+  box.position.set(0, 1, -3);
+  box.rotation.set(
+    0,
+    THREE.MathUtils.radToDeg(45),
+    THREE.MathUtils.radToDeg(45)
+  );
+  scene.add(box);
+  box.onSelectDown = () => {
+    if (box.material.color.getHex() === 0x668cff) {
+      box.material.setValues({ color: 0xff6347 });
+    } else {
+      box.material.setValues({ color: 0x668cff });
+    }
+  };
+
+  camera.position.set(0.0, 1.6, 0);
+  const cameraContainer = new THREE.Object3D();
+  cameraContainer.add(camera);
+  const player = new THREE.Object3D();
+  player.add(cameraContainer);
+  scene.add(player);
+
+  const interactableObjects = [box];
+  const teleportTargetObjects = [ground];
+  const collisionBoxes = [...teleportTargetObjects].map((o) =>
+    new THREE.Box3().setFromObject(o)
+  );
+
+  setupVerse(
+    scene,
+    renderer,
+    camera,
+    cameraContainer,
+    player,
+    collisionBoxes,
+    [],
+    interactableObjects,
+    teleportTargetObjects
+  ).then((tick) => {
+    ticks.push(tick);
+  });
+}
+
+main();
